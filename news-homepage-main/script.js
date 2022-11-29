aca5259f92b39cad299beb1d0547c6c539096e45
Animashaun Raheem <99181713+constneophyte@users.noreply.github.com>
2022-11-29 22:45:30 +0100

    Delete news-homepage-main directory

--- news-homepage-main/script.js
+++
@@ -1,9 +0,0 @@
-const hamMenu = document.querySelector('.hamburgerMenu');
-const offScreen = document.querySelector('.offScreen');
-const body = document.querySelector('body');
-
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreen.classList.toggle('active');
-    body.classList.toggle('active');
-});
